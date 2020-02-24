974cdd5d937c6aeda419b2f209188598a46c5cf7
Amir Zadeh <abagherz@cs.cmu.edu>
2020-02-23 20:42:44 -0500

    Update cmu_mosei.py

--- mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSEI/cmu_mosei.py
+++ mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSEI/cmu_mosei.py
@@ -8,8 +8,7 @@ highlevel["OpenFace2.0"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSEI/visual/
 highlevel["OpenSMILE"]="http://immortal.multicomp.cs.cmu.edu/CMU-MOSEI/acoustic/CMU_MOSEI_openSMILE_IS09.csd"
 highlevel["FACET 4.2"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSEI/visual/CMU_MOSEI_VisualFacet42.csd'
 labels={}
-labels["Sentiment Labels"]="http://immortal.multicomp.cs.cmu.edu/CMU-MOSEI/labels/CMU_MOSEI_LabelsSentiment.csd"
-labels["Emotion Labels"]="http://immortal.multicomp.cs.cmu.edu/CMU-MOSEI/labels/CMU_MOSEI_LabelsEmotions.csd"
+labels["All Labels"]="http://immortal.multicomp.cs.cmu.edu/CMU-MOSEI/labels/CMU_MOSEI_All_Labels.csd"
 extra={}
 extra["glove_vectors_with_sp"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSEI/language/CMU_MOSEI_TimestampedGloveVectors_with_SP.csd'
 
