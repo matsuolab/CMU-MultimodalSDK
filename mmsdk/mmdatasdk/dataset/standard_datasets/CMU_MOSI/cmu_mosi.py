dd0b4ea7d8afb73f75f234ce7e8c75687fd8afbd
Amir Zadeh <abagherz@cs.cmu.edu>
2020-07-13 02:04:12 -0400

    Update cmu_mosi.py

--- mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSI/cmu_mosi.py
+++ mmsdk/mmdatasdk/dataset/standard_datasets/CMU_MOSI/cmu_mosi.py
@@ -3,16 +3,15 @@ raw["words"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_Ti
 raw["phonemes"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedPhones.csd'
 
 highlevel={}
-#BERT and glove will be back soon with the newest version of forced alignment - you can extract yours 
-#but make sure BERT is extracted per segment and not per video. Otherwise your method will have more information 
-#beyond just segment. 
-highlevel["glove_vectors"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedWordVectors_1.1.csd'
-highlevel["FACET 4.1"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/visual/CMU_MOSI_VisualFacet_4.1.csd'
+highlevel["glove_vectors"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/language/CMU_MOSI_TimestampedWordVectors.csd'
+highlevel["FACET_4.1"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/visual/CMU_MOSI_Visual_Facet_41.csd'
+highlevel["FACET_4.2"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/visual/CMU_MOSI_Visual_Facet_42.csd'
 highlevel["OpenSmile-emobase2010"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/acoustic/CMU_MOSI_OpenSmile_EB10.csd'
 highlevel["OpenSMILE"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/acoustic/CMU_MOSI_openSMILE_IS09.csd'
-highlevel["OpenFace"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/visual/CMU_MOSI_OpenFace2.csd'
+highlevel["OpenFace_1"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/visual/CMU_MOSI_OpenFace1.csd'
+highlevel["OpenFace_2"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/visual/CMU_MOSI_OpenFace2.csd'
 highlevel["COVAREP"]='http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/acoustic/CMU_MOSI_COVAREP.csd'
-#covarep to be added
+
 labels={}
 labels["Opinion Segment Labels"]="http://immortal.multicomp.cs.cmu.edu/CMU-MOSI/labels/CMU_MOSI_Opinion_Labels.csd"
 
